aa9f7ab4c5849fc8afe7fc7f6d23322d1eb638de
Vinod Kumar Gupta <vkgupta857@gmail.com>
2020-05-28 21:07:58 +0530

    Added SQLite db

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 
 # import required at this place
 from app import routes
---
+++ config.py
@@ -0,0 +1,4 @@
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
