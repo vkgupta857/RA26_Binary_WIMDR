8eede87dc5232f9a978fb34be94b0b856bf5d607
Vinod Kumar Gupta <vkgupta857@gmail.com>
2020-07-06 20:47:02 +0530

    removed test uri from routes

--- app/routes.py
+++ app/routes.py
@@ -5,6 +5,11 @@ from werkzeug.utils import secure_filename
 
 from app import app
 
+def allowed_file(filename):
+    ALLOWED_EXTENSIONS = ['jpg', 'png']
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -31,7 +36,7 @@ def upload():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            flash(place+lat+lng, category="primary")
+            flash("Image uploaded successfully", category="success")
             return render_template('uploaded_file.html',filename=filename)
 
         else:
@@ -55,43 +60,6 @@ def week():
 @app.route('/piechart')
 def piechart():
     return render_template('piechart.html')
-
-
-def allowed_file(filename):
-    ALLOWED_EXTENSIONS = ['jpg', 'png']
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route('/test', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        print(file)
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return render_template('uploaded_file.html',filename=filename)
-        else:
-            flash("Invalid file")
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
-
 
 ##############################################
 # API Section starts here
