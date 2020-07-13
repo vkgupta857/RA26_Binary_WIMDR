7d450d17c088f48749a30adb4ac06c64f9f7ccb1
Vinod Kumar Gupta <vkgupta857@gmail.com>
2020-07-13 09:44:25 +0530

    app.py: one file for everything - merged all files to app.py

--- app.py
+++ app.py
@@ -1,4 +1,99 @@
-from app import app
+import os
+import json
+from flask import Flask, render_template, request, flash, redirect, url_for
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+##################
+# Config Section
+##################
+
+app.config['SECRET_KEY'] = "MySecretKeyDontCopy"
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+
+######################
+# App Routes Section
+######################
+
+def allowed_file(filename):
+    '''
+    Function to check file types
+    '''
+    ALLOWED_EXTENSIONS = ['jpg', 'png']
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['GET','POST'])
+def upload():
+    # If request method is POST
+    if request.method == 'POST':
+        lat = request.form['latitude']
+        lng = request.form['longitude']
+        # check if the post request has the image
+        if 'image' not in request.files:
+            flash('No file uploaded')
+            return redirect(request.url)
+
+        file = request.files['image']
+        # if request with no file
+        if file.filename == '':
+            flash('No image attached. Please attach an image of waste.',category="danger")
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash("Image uploaded successfully", category="success")
+            return render_template('uploaded_file.html',filename=filename)
+
+        else:
+            flash("Invalid file type", category="danger")
+            return redirect(request.url)
+
+    # If request method is other than POST e.g. GET
+    return render_template('upload.html')
+
+@app.route('/update')
+def update():
+    return render_template('update.html')
+
+@app.route('/view')
+def view():
+    return render_template('view.html')
+
+@app.route('/week')
+def week():
+    return render_template('week.html')
+
+@app.route('/piechart')
+def piechart():
+    return render_template('piechart.html')
+
+###############################################
+# API Routes Section for Android app and AJAX requests
+###############################################
+
+@app.route('/api/json')
+def api_json():
+    response = {}
+    response['data'] = ['1','2','3']
+    return json.dumps(response)
+
+@app.route('/api/report/weekly')
+def api_report_weekly():
+    response = {}
+    response['data'] = ['1','2','3']
+    return json.dumps(response)
+
+###########
+# Run App
+###########
 
 if __name__ == '__main__':
    app.run(debug=True)
--- config.py
+++
@@ -1,8 +0,0 @@
-
-class Config:
-
-    SECRET_KEY = "My"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    UPLOAD_FOLDER = "app/static/uploads"
