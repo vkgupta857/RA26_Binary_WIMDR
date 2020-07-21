ad21abcdb6cd88dacb1f31f75166f5a83c05f48a
Prashant Jha <prashantjha.jec@gmail.com>
2020-07-22 02:23:23 +0530

    Cnfigured project for GCP.
    - Added database connection code to main file.
    - Added app.yaml file.
    - Deleted Procfile.
    - Deleted keras_model.h5

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 import json
 from flask import Flask, render_template, request, flash, redirect, url_for
 from werkzeug.utils import secure_filename
-#import waste_classification as wc
+# import waste_classification as wc
 from datetime import datetime
 # for getting state and district
 import reverse_geocoder as rg
@@ -14,6 +14,59 @@ import urllib.request
 import random
 import employee_id
 import requests as req
+# for database connectivity
+import sqlalchemy
+
+
+# Remember - storing secrets in plaintext is potentially unsafe. Consider using
+# something like https://cloud.google.com/kms/ to help keep secrets secret.
+db_user = os.environ.get("DB_USER")
+db_pass = os.environ.get("DB_PASS")
+db_name = os.environ.get("DB_NAME")
+cloud_sql_connection_name = os.environ.get("CLOUD_SQL_CONNECTION_NAME")
+
+# [START cloud_sql_mysql_sqlalchemy_create]
+# The SQLAlchemy engine will help manage interactions, including automatically
+# managing a pool of connections to your database
+db = sqlalchemy.create_engine(
+    # Equivalent URL:
+    # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=/cloudsql/<cloud_sql_instance_name>
+    sqlalchemy.engine.url.URL(
+        drivername="mysql+pymysql",
+        username=db_user,
+        password=db_pass,
+        database=db_name,
+        query={"unix_socket": "/cloudsql/{}".format(cloud_sql_connection_name)},
+    ),
+    # ... Specify additional properties here.
+    # [START_EXCLUDE]
+    # [START cloud_sql_mysql_sqlalchemy_limit]
+    # Pool size is the maximum number of permanent connections to keep.
+    pool_size=5,
+    # Temporarily exceeds the set pool_size if no connections are available.
+    max_overflow=2,
+    # The total number of concurrent connections for your application will be
+    # a total of pool_size and max_overflow.
+    # [END cloud_sql_mysql_sqlalchemy_limit]
+    # [START cloud_sql_mysql_sqlalchemy_backoff]
+    # SQLAlchemy automatically uses delays between failed connection attempts,
+    # but provides no arguments for configuration.
+    # [END cloud_sql_mysql_sqlalchemy_backoff]
+    # [START cloud_sql_mysql_sqlalchemy_timeout]
+    # 'pool_timeout' is the maximum number of seconds to wait when retrieving a
+    # new connection from the pool. After the specified amount of time, an
+    # exception will be thrown.
+    pool_timeout=30,  # 30 seconds
+    # [END cloud_sql_mysql_sqlalchemy_timeout]
+    # [START cloud_sql_mysql_sqlalchemy_lifetime]
+    # 'pool_recycle' is the maximum number of seconds a connection can persist.
+    # Connections that live longer than the specified amount of time will be
+    # reestablished
+    pool_recycle=1800,  # 30 minutes
+    # [END cloud_sql_mysql_sqlalchemy_lifetime]
+    # [END_EXCLUDE]
+)
+# [END cloud_sql_mysql_sqlalchemy_create]
 
 
 app = Flask(__name__)
@@ -90,23 +143,23 @@ def upload():
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             flash("Image uploaded successfully", category="success")
-            
+
             #classification
             url = 'https://waste-classification-api.el.r.appspot.com/' + filename
             status = req.get(url).text
-            
+
             # getting date and time
             d,t = get_date_time()
-            
+
             # getting state and district
             district, state = get_place(lat, lng)
-            
+
             # gettig emp_id
             try:
                 emp_id = employee_id.emp_id[state][district]
             except:
                 emp_id = 0
-            
+
             # adding report to the database
             data = {'Report_No' : str(random.randint(10**4,10**5)),
                     'State' : str(state),
@@ -121,10 +174,10 @@ def upload():
                     'Resolved' : str(0),
                     'Emp_ID' : str(emp_id),
                     'Image' : str(filename)}
-            
+
             ref = db.reference('Pending_Reports')
             ref.push(data)
-            
+
             return render_template('uploaded_file.html',filename=filename, label=status)
 
         else:
